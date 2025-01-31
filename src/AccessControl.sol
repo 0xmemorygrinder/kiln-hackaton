aea5b1921cf4b2fd34caaeb34715cd038a937e8b
0xMemoryGrinder <35138272+0xMemoryGrinder@users.noreply.github.com>
2025-01-31 20:14:01 +0100

    feat: add operator role and getter

--- src/AccessControl.sol
+++ src/AccessControl.sol
@@ -11,13 +11,16 @@ contract AccessControl is OwnableRoles {
     address public curator;
     address public treasury;
 
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
     constructor(
         address _metaVault,
         address _staker,
         address _teller,
         address _accountant,
         address _curator,
-        address _treasury
+        address _treasury,
+        address _initialOperator,
     ) {
         metaVault = _metaVault;
         staker = _staker;
@@ -25,6 +28,7 @@ contract AccessControl is OwnableRoles {
         accountant = _accountant;
         curator = _curator;
         treasury = _treasury;
+        grantRole(OPERATOR_ROLE, _initialOperator);
     }
     
     function isCurator(address account) external view returns (bool) {
@@ -33,5 +37,9 @@ contract AccessControl is OwnableRoles {
 
     function isTreasury(address account) external view returns (bool) {
         return account == treasury;
+    }
+
+    function isOperator(address account) external view returns (bool) {
+        return hasRole(OPERATOR_ROLE, account);
     }
 }
